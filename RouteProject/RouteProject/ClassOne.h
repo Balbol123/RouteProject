8ff780ef8d760f9ef0c8ce9b1cc5297cb11bfe20
Balbol123 <90799975+Balbol123@users.noreply.github.com>
2022-09-07 18:01:09 +0200

    Add files via upload

---
+++ RouteProject/RouteProject/ClassOne.h
@@ -0,0 +1,11 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+#pragma once
+class ClassOne {
+  
+};
+
---
+++ RouteProject/RouteProject/RouteProject.cpp
@@ -0,0 +1,12 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <string>
+#include <cmath>
+#include "ClassOne.h"
+using namespace std;
+
+int main() {
+    ClassOne co;
+    cout << "Testing" << endl;
+    return 0;
+}
